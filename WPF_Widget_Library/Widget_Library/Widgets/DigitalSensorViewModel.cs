49d19050723aa8334cdb1d6e6df68d722cbda9c1
DotNetTutorial <85148810+roy1117@users.noreply.github.com>
2025-03-11 01:06:20 +0900

    Added first widget
    Added Digital Sensor Widget

---
+++ WPF_Widget_Library/Widget_Library/Widgets/DigitalSensorViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.ComponentModel;
+
+namespace Widget_Library.Widgets
+{
+    public class DigitalSensorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Brush _strokeColor;
+        public Brush StrokeColor
+        { 
+            get
+            {
+                return _strokeColor;
+            }
+            set
+            {
+                _strokeColor = value;
+                OnPropertyChanged(nameof(StrokeColor));
+            }
+        }
+
+        public DigitalSensorViewModel()
+        {
+            StrokeColor = Brushes.Black;
+        }
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
